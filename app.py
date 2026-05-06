0bb70cc4863b37ed15b545130b5394546450b73c
riohighkick-droid <rio.highkick@gmail.com>
2026-05-07 03:21:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def play_player_match(home_player, away_player):
 
     for i in range(3):
 
-        home_card = draw_card(home_player)
-        away_card = draw_card(away_player)
+        home_card = random.choice(players[home_player]["cards"])
+        away_card = random.choice(players[away_player]["cards"])
 
         result = judge_battle(home_card, away_card)
 
