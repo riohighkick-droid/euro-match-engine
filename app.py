18da3bf947fede70a4b49c2094ea0e207c7baa42
riohighkick-droid <rio.highkick@gmail.com>
2026-05-07 02:48:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def play_player_match(home_player, away_player):
             losers.append(home_player)
 
         else:
-            append("→ DRAW")
+            battle_logs.append("→ DRAW")
 
     if home_wins > away_wins:
         return "home",  home_card, losers
