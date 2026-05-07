935ac6f896de6d68663e7515e27950533959824c
riohighkick-droid <rio.highkick@gmail.com>
2026-05-07 21:29:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def pick_side(side_label, side_icon):
         logo_col1, logo_col2, logo_col3 = st.columns([1,2,1])
 
         with logo_col2:
-            st.markdown("<div style='height:170px; padding-top:45px;'>", unsafe_allow_html=True)
+            st.markdown("<div style='height:170px; padding-top:15px;'>", unsafe_allow_html=True)
             st.image(logo_file, width=100)
             st.markdown("</div>", unsafe_allow_html=True)
         
